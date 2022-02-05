906a2a0cfe865889695fc2d41ad2cc96896bacfe
philip howley <howley.phil@gmail.com>
2022-02-04 19:42:39 -0500

    Add spread operator notes

--- README.md
+++ README.md
@@ -1,25 +1,41 @@
-# Computed Property Names
-* ES6 feature that allows you to have an expression be computed as a property name on an object
+# Spread syntax
+* Introduced in ES6 to help make our code cleaner and easier to understand
 
-## What is an expression in JavaScript again?
-* A piece of code that results in a single value (like a variable or function call (aka invokation))
+## What is the spread operator?
+* Just three dots `...`
 
-# Example
-* See index.js
+## What can the spread operator be used on?
+* iterables like an array or a string
+
+## What does it do?
+* It expands an iterable to it's individual elements
+* It can provide a function call with an array (or any other iterable) where 0 or more arguments were expected
+
+### Example
+* See EXAMPLE1 in index.js
+
+## Where can we use the Spread Operator?
+* Inside function calls (when using this we call it the "rest parameter")
+* Creating/extending an array/iterables
+* Used as an argument for a "variadic function" (a function which can have a variable number of parameters)
+
+### Deeper Dive
+
 
 ## Git Branches
 * main
 * 01-regex 
 * 02-ternary-operator
-* 03-compted-prop-names (now)
-* 03-spread-syntax (next)
+* 03-compted-prop-names 
+* 04-spread-syntax (now)
+* 05-controlled-forms (next)
 
 ## Topics for Review Today
 * Regular expressions 
 * Ternary operator
-* Computed property names (now)
-* Spread syntax (next)
-* Controlled forms
+* Computed property names
+* Spread syntax (now)
+* Controlled forms (next)
 * Binding event handlers
 * Uncontrolled forms
 * Form validation
@@ -27,4 +43,5 @@
 * React Redux Form & Validation
 
 ## Resource
-* [more info on JS computed properties](https://medium.com/dailyjs/how-to-use-javascript-computed-properties-8f6f096379e3)
+* [What is the difference between an Array and Array Iterator in JavaScript](https://stackoverflow.com/questions/58667005/what-is-the-difference-of-a-real-array-and-array-iterator-in-js) 
+* [intro to spread operator in js](https://www.freecodecamp.org/news/an-introduction-to-spread-syntax-in-javascript-fba39595922c/)
--- index.js
+++ index.js
@@ -1,27 +1,97 @@
-// 1. Create a function that takes in two arguments "key" and "value"
-// 2. This function returns an object using those arguments
-// 3. How can you do this?
-
-// function objectify(key, value) {
-//   let obj = {}
-//   obj[key] = value
-//   return obj
+// EXAMPLE 1
+// BEFORE SPREAD OPERATOR IN ES6
+// WE HAD TO USE APPLY METHOD LIKE THIS
+// function sum(x, y, z) {
+//   return x + y + z
 // }
 //
-// console.log(objectify('title', 'The Sun Also Rises'))
-
-// but computer property names make life easier for us
-// 1. We can use object literal notation
-// 2. and assign the property as an object (without having to create it first)
-
-function objectify(key, value) {
-  return {
-    [key]: value,
-  }
-}
-
-console.log(objectify('title', 'The Sun Also Rises'))
-
-// NOTE: Here the key argument can be ay expression as long as it is wrapped in brackets []
+// const numbers = [1, 2, 3]
 //
-// see the practical example code for a practical example :)
+// // console.log(sum.apply(null, numbers)) // 6
+//
+// NOW WITH SPREAD OPERATOR
+//
+// console.log(sum(...numbers)) // 6
+
+// EXAMPLE 2
+// INSIDE FUNCTION CALLS (AKA REST PARAMETER)
+// function someFunc() {}
+// func(...argument)
+
+// CREATING/EXTENDING AN ARRAY/ITERABLE
+// const arr = ['one', 'two']
+// const newArr = [...arr, 'aa', 'bb', 'cc']
+// console.log(newArr)
+
+// function sum(...args) {
+//   return args
+// }
+// const numbers = [1, 2, 3]
+// console.log(sum(...numbers))
+
+// HOW TO GET THE SUM OF ALL THE ARGUMENTS
+// function sum(...args) {
+//   return args.reduce((acc, a) => acc + a, 0)
+// }
+// const numbers = [1, 2, 3]
+// console.log(sum(...numbers)) // 6
+
+// PUSH ELEMENTS INTO ARRAY
+// const arr = [1, 2]
+// arr.push([1, 2, 3])
+// console.log(arr) // [1, 2, [1,2,3] ]
+// limitation of push is we have to push in elements one by one
+// but pushing an array into an existing array we get the unwanted result of a multidimensional array
+
+// OLD SOLUTION (AGAIN USING APPLY() METHOD)
+// const arr = [1, 2]
+// arr.push.apply(arr, [1, 2, 3])
+// console.log(arr) // [1,2,1,2,3]
+// NOT A GREAT SOLUTION
+// NOT VERY READABLE
+
+// A BETTER MORE ELEGANT READABLE WAY WITH REST
+// const arrOne = [1, 2]
+// const arrTwo = [1, 2, 3]
+// arrOne.push(...arrTwo)
+// console.log(arrOne) // [1,2,1,2,3]
+
+// COPY AN ARRAY
+// let arrOne = [1, 2]
+// const arrTwo = [1, 2, 3]
+// arrOne = [...arrTwo]
+// console.log(arrOne) // [1,2,3]
+
+// WE CAN COPY USING OBJECTS
+// const a = { a: 1, b: 2 }
+// const obj = { ...a }
+// console.log(obj) // { a: 1, b: 2}
+
+// CONCATENATING 2 ARRAYS
+// let arrOne = [1, 2]
+// const arrTwo = [1, 2, 3]
+// console.log(arrOne.concat(arrTwo)) // [1,2,1,2,3]
+
+// BUT WE CAN DO THE SAME AS ABOVE USING THE `...` OPERATOR
+// NOTE: THREE DOTS IN JAVASCRIPT) IS CALLED THE SPREAD SYNTAX OR SPREAD OPERATOR
+// let arrOne = [1, 2]
+// const arrTwo = [1, 2, 3]
+// console.log([...arrOne, ...arrTwo]) // [1,2,1,2,3]
+
+// CONVERT A STRING TO AN ARRAY
+// const greet = 'Hello'
+// const greetArr = greet.split('')
+// console.log(greetArr) //[ 'H', 'e', 'l', 'l', 'o' ]
+
+// FIND MAX NUMBER IN AN ARRAY
+// THIS DOESN'T WORK
+// const numsArr = [1, 3, 40, 50]
+// console.log(Math.max(numsArr)) // NaN
+
+// WE COULD SOLVE USING APPLY() BUT SPREAT OPERATOR IS BETTER
+// const numsArr = [1, 3, 40, 50]
+// console.log(Math.max(...numsArr)) // 50
+
+// MIN WORKS TOO!
+// const numsArr = [1, 3, 40, 50]
+// console.log(Math.min(...numsArr)) // 1
