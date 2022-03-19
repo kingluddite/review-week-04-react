f05613690d7c4adce50ad0b303634ccc0b1938e3
philip howley <howley.phil@gmail.com>
2022-03-19 10:54:02 -0400

    Fix comment misspelled error

--- index.js
+++ index.js
@@ -7,20 +7,20 @@
 //   obj[key] = value
 //   return obj
 // }
-//
-// console.log(objectify('title', 'The Sun Also Rises'))
+// //
+// console.log(objectify("title", "The Sun Also Rises"))
 
-// but computer property names make life easier for us
+// but computed property names make life easier for us
 // 1. We can use object literal notation
 // 2. and assign the property as an object (without having to create it first)
 
-function objectify(key, value) {
-  return {
-    [key]: value,
-  }
-}
+// function objectify(key, value) {
+//   return {
+//     [key]: value,
+//   }
+// }
 
-console.log(objectify('title', 'The Sun Also Rises'))
+// console.log(objectify('title', 'The Sun Also Rises'))
 
 // NOTE: Here the key argument can be ay expression as long as it is wrapped in brackets []
 //
